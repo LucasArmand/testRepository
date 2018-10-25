b36a78603ab253097f273748b2087a18fe63eda4
LucasArmand <40708637+LucasArmand@users.noreply.github.com>
2018-10-25 13:03:57 -0400

    Add files via upload
    Craps

---
+++ src/Craps.java
@@ -0,0 +1,56 @@
+//Lucas Armand
+//Computer Science Period 5
+
+import java.util.Scanner;
+
+public class Craps {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Dice d1 = new Dice();
+		Dice d2 = new Dice();
+		int roll1,roll2;
+		int point = 0;
+		
+		do {
+			System.out.print("Press 'enter' to roll the dice...");
+			in.nextLine();
+			roll1 = d1.roll();
+			roll2 = d2.roll();
+			System.out.println("Rolled " + roll1 + " and " + roll2);
+			point = roll1 + roll2;
+			
+			mainSwitch:
+			switch (point) {
+			case 7:
+				System.out.println("Congrats, you rolled 7, with " + roll1 + " and " + roll2 + ", so you win!");
+				break;
+			case 11:
+				System.out.println("Congrats, you rolled 11, with " + roll1 + " and " + roll2 + ", so you win!");
+				break;
+			case 2:
+				System.out.println("Sorry, you rolled 2, with " + roll1 + " and " + roll2 + ", so you lose");
+				break;
+			case 12:
+				System.out.println("Sorry, you rolled 12, with " + roll1 + " and " + roll2 + ", so you lose");
+				break;
+			default:
+				System.out.println("Point set at " + point);
+				do {
+					System.out.print("Press 'enter' to roll the dice...");
+					in.nextLine();
+					roll1 = d1.roll();
+					roll2 = d2.roll();
+					System.out.println("Rolled " + roll1 + " and " + roll2);
+					if (roll1 + roll2 == 7) {
+						System.out.println("Sorry, you rolled 7, with " + roll1 + " and " + roll2 + ", so you lose");
+						break mainSwitch;
+					}
+				}while(roll1 + roll2 != point);
+				System.out.println("Congrats, you rolled your point of " + point+ ", with " + roll1 + " and " + roll2 + ", so you win!");
+				break;
+			}	
+			System.out.println("Play Again? Y/N: ");
+		}while(in.nextLine().toUpperCase().equals("Y"));
+	}
+}
